2c6bc8027fcaef03cc23ed517f4f2672b2fbd03c
Krisi <quanchen2047@gmail.com>
2019-07-07 17:52:55 +0900

    selenium: add lint/format settings, and format code

--- just-for-test/src/selenium/justPlay/play/play.py
+++ just-for-test/src/selenium/justPlay/play/play.py
@@ -1,40 +1,58 @@
-import sys 
-sys.path.append (r'F:/Code/JustFor_Practice/justfor_practice/just-for-test/src/selenium/') 
+"""A test selenium example to craw good pictures"""
 
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import sys
+sys.path.append(
+    r'F:/Code/JustFor_Practice/justfor_practice/just-for-test/src/selenium/')
 from justPlay.util.util import find_element_safe, KEYWORD
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import dateutil.parser as dparser
+import datetime
+import time
 
+datefile_root_path = "F:/New Folder/新しいフォルダー (7)/aisinei/"
 lib_for_search = "XIAOYU语画界"
+save_path = ""
+date_file = open(datefile_root_path + lib_for_search + "/date.txt", "r")
+base_date = dparser.parse(date_file.read(), fuzzy=True)
 
-driver = webdriver.Chrome(r'F:/Code/reference/chromedriver/chromedriver.exe')  # Optional argument, if not specified will search path.
+# Optional argument, if not specified will search path.
+driver = webdriver.Chrome(r'F:/Code/reference/chromedriver/chromedriver.exe')
 driver.get('https://www.aisinei.net/')
-time.sleep(1) # Let the user actually see something!
-# go to the 
-target_button = driver.find_element(By.XPATH, 
-  "//td[contains(@class,'fl_g')]//a[contains(text(),'{}')]".format(lib_for_search))
+# go to the targeted lib
+target_button = driver.find_element(
+    By.XPATH, "//td[contains(@class,'fl_g')]//a[contains(text(),'{}')]".format(lib_for_search))
 target_button.click()
 
 # time.sleep(1)
-# new_window_button = driver.find_element(By.XPATH, 
+# new_window_button = driver.find_element(By.XPATH,
 #   "//span[@id='atarget']")
 # new_window_button.click()
 
 time.sleep(1)
-image_mode_button = driver.find_element(By.XPATH, 
- "//a[@class='chked']")
+image_mode_button = driver.find_element(By.XPATH,
+                                        "//a[@class='chked']")
 if image_mode_button:
     image_mode_button.click()
 
 while True:
     paging_box = find_element_safe(driver, By.XPATH, "//a[@class='nxt']")
     if paging_box:
-        task_list = driver.find_elements(By.XPATH, "//div[@class='posttitle']//a[@class='st']")
-
+        task_list = driver.find_elements(
+            By.XPATH, "//div[@class='posttitle']//a[@class='st']")
+        true_task_list = []
         for task in task_list:
-            folder_name = task.text
-            print("Current Crawing {}".format(folder_name))
+            task_text = task.text
+            task_date = dparser.parse(task_text, fuzzy=True).date()
+            if task_date > base_date:
+                true_task_list.append(task)
+                print(task_text)
+
+        if not true_task_list:
+            break
+
+        for task in true_task_list:
+            print("Current Crawing {}".format(task_text))
             task.click()
 
             time.sleep(1)
@@ -43,14 +61,17 @@ while True:
             print(tabs)
             driver.switch_to.window(tabs[1])
 
-            describe_text = driver.find_element(By.XPATH, "//td[contains(@id,'postmessage')]")
+            describe_text = driver.find_element(
+                By.XPATH, "//td[contains(@id,'postmessage')]")
             print(describe_text.text)
 
-            desc_text = describe_text.text.split("[套图简介]",1)[1].split("[会员下载]",1)[0]
+            desc_text = describe_text.text.split(
+                "[套图简介]", 1)[1].split("[会员下载]", 1)[0]
             is_target = any(key_word in desc_text for key_word in KEYWORD)
-            if (is_target):
-              big_pic_btn = driver.find_element(By.XPATH, "//a[@class='xi2 attl_m']")
-              big_pic_btn.click()
+            if is_target:
+                big_pic_btn = driver.find_element(
+                    By.XPATH, "//a[@class='xi2 attl_m']")
+                big_pic_btn.click()
 
             time.sleep(3)
             driver.close()
@@ -58,7 +79,7 @@ while True:
 
             # time.sleep(1)
             # driver.back()
-        
+
         paging_box.click()
         time.sleep(1)
     else:
@@ -69,7 +90,5 @@ time.sleep(2)
 driver.back()
 # search_box.send_keys('ChromeDriver')
 # search_box.submit()
-time.sleep(5) # Let the user actually see something!
+time.sleep(5)  # Let the user actually see something!
 driver.quit()
-
-
--- just-for-test/src/selenium/justPlay/util/util.py
+++ just-for-test/src/selenium/justPlay/util/util.py
@@ -1,11 +1,19 @@
-
+"""util functions for the selenium example
+"""
 from selenium.common.exceptions import NoSuchElementException
 
 
 def find_element_safe(driver, option, regex):
+    """safely search for elements.
+
+    Returns:
+        [webdriver.element]: [found element]
+        [null]: [no result]
+    """
     try:
         return driver.find_element(option, regex)
     except NoSuchElementException:
         return None
+
 
 KEYWORD = ["黑丝", "丝袜 "]
